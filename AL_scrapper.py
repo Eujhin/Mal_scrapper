7e27919d84a772246192bcc3629be2967db164ba
Eujhin <eugeniogod@gmail.com>
2022-07-03 16:14:18 +0100

    get table

--- AL_scrapper.py
+++ AL_scrapper.py
@@ -7,4 +7,16 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 #print(soup.prettify())
 
-list(soup.children)
+html = list(soup.children)[2]
+
+body = list(html.children)[3]
+
+l_cont = list(body.children)[5]
+
+l_block = list(l_cont)[5]
+
+l_all_anime = list(l_block)[1]
+
+table = list(l_all_anime)[5]
+
+print("yo")
